25938b173de2b26c28af9507e159d8f195248e78
Липатов Максим <103856799+aovent@users.noreply.github.com>
2023-05-25 13:28:18 +0300

    Create renderFunctions.js

---
+++ modules/renderFunctions.js
@@ -0,0 +1,61 @@
+const main = document.querySelector("main");
+
+export async function renderPosts(array, _activeButtonID) {
+    setActiveButton(_activeButtonID);
+    
+    array.forEach(item => {
+        renderPost(item.title, item.body, item.id);
+    })
+} 
+
+export async function renderPage() {
+    let pageSection = document.createElement("section");
+    let pageButtons = document.createElement("section");
+
+    main.append(pageSection);
+    main.append(pageButtons);
+
+    pageSection.classList.add("page_section");
+    pageButtons.classList.add("page_buttons");
+    
+    let button;
+    for (let index = 1; index < 11; index++) {
+        button = document.createElement("button");
+
+        button.innerHTML = index;
+
+        pageButtons.append(button);
+    }
+}
+
+async function renderPost(_title, _body, _id) {
+    let pageSection = document.querySelector(".page_section");
+
+    let postDiv = document.createElement("div");
+    postDiv.classList.add("post_div");
+    pageSection.append(postDiv);
+
+    let h1title = document.createElement("a");
+    h1title.classList.add("post_title");
+    h1title.innerHTML = _title;
+    h1title.href = `post.html?id=${_id}`;
+    h1title.target = "_blank"
+    postDiv.append(h1title);
+
+    let postBody = document.createElement("div");
+    postBody.classList.add("post_body");
+    postBody.innerHTML = _body;
+    postDiv.append(postBody);
+}
+
+async function setActiveButton(_id) {
+    let allButtons = document.querySelectorAll("button");
+
+    allButtons.forEach((item, index) => {
+        if (index != _id - 1) {
+            item.classList.remove("active_button");
+        }
+    })
+
+    allButtons[_id - 1].classList.add("active_button");
+}
